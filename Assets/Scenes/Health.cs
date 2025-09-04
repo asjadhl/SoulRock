12b7637bfc23aed54206adbcceecf19be7ddea28
swonkim64-svg <swonkim64@gmail.com>
2025-09-04 12:43:13 +0900

    Health

---
+++ Assets/Scenes/Health.cs
@@ -0,0 +1,54 @@
+using System.ComponentModel;
+using UnityEditor;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    public void Awake()
+    {
+        InstanteHealthBar();
+    }
+
+
+
+    void InstanteHealthBar()
+    {
+        GameObject rectHealthBar = new GameObject();
+    }
+}
+
+
+
+[CustomEditor(typeof(Health))]
+public class Test: Editor
+{
+
+    public GUIStyle centeredStyle;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        serializedObject.Update();
+
+        centeredStyle = new GUIStyle(centeredStyle)
+        {
+            alignment = TextAnchor.MiddleCenter,
+            fontStyle = FontStyle.Bold,
+            fontSize = 13
+        };
+
+
+        GUILayout.Label("HEALTH-BAR-Rect");
+
+        GUILayout.BeginHorizontal();
+
+
+
+        GUILayout.EndHorizontal();
+
+    }
+
+
+
+}
