c78d7ee7288481a0b2acfb4b79e2b2a8c98ff3f4
asjadhl <yunjiwoo0518@gmail.com>
2025-09-04 21:09:35 +0900

    PlayerMoveToCloseEnemy

--- Assets/YJW/Scripts/PlayerMoveToCloseEnemy.cs
+++ Assets/YJW/Scripts/PlayerMoveToCloseEnemy.cs
@@ -5,13 +5,5 @@ public class PlayerMoveToCloseEnemy : MonoBehaviour
     public GameObject enemyPrefab;
 
     
-    private void PlayerMove()
-    {
-        GameObject newEnemy = Instantiate(enemyPrefab);
-        EnemyTest.EnemySpawned(newEnemy);
-    }
-    public void HandleEnemySpawned(GameObject enemy)
-    {
-        PlayerMove();
-    }
+    
 }
