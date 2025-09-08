8eb8785919e2f986ee4fc8250d17e742b3236d04
asjadhl <yunjiwoo0518@gmail.com>
2025-09-08 16:06:40 +0900

    test

--- Assets/YJW/Scripts/BossShoot.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class BossShoot : MonoBehaviour
-{
-    private float shootTimer;
-}
--- Assets/YJW/Scripts/PlayerShoot.cs
+++ Assets/YJW/Scripts/PlayerShoot.cs
@@ -5,16 +5,16 @@ public class PlayerShoot : MonoBehaviour
     [SerializeField] Camera camera;
     private void Update()
     {
-        PlayerShoot_();
+
     }
 
-    private void PlayerShoot_()
+    public void PlayerShoot_()
     {
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, 100f))
+            if (Physics.Raycast(ray, out hit, 100f) && hit.collider.gameObject.tag == "Enemy")
             {
                 Destroy(hit.collider.gameObject);
             }
