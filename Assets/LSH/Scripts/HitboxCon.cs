b1439cc02eb2c617661b42a49c032a8a9505ca22
asjadhl <yunjiwoo0518@gmail.com>
2025-09-09 10:48:29 +0900

    hitbox + player

--- Assets/LSH/Scripts/HitboxCon.cs
+++ Assets/LSH/Scripts/HitboxCon.cs
@@ -13,6 +13,7 @@ public class HitBoxCon: MonoBehaviour
     public Transform leftDotBox;
     public Transform rightDotBox;
 
+    [SerializeField] GameObject player;
     private void Update()
     {
         
@@ -50,7 +51,7 @@ public class HitBoxCon: MonoBehaviour
 
     private void OnClickSuccess()
     {
-        
+        player.GetComponent<PlayerShoot>().PlayerShoot_();
             Debug.Log("클릭 성공!");
         
         // 클릭 성공 시 처리할 로직
