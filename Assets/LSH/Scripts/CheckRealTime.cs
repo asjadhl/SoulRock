2b5de17322f13e5502779055534184b1ce498b22
도환 <dltkdguer@gmail.com>
2025-09-17 10:07:53 +0900

    inGameREaltiEM

---
+++ Assets/LSH/Scripts/CheckRealTime.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CheckRealTime : MonoBehaviour
+{
+    public static CheckRealTime Instance { get; private set; }
+
+    double inGamerealTime = 0;
+    void Awake()
+    {
+        // ΩÃ±€≈Ê
+        if (Instance == null) Instance = this;
+        else if (Instance != this) Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inGamerealTime = AudioSettings.dspTime - DotBoxGeneratorL.Instance.musicStartDspTime;
+    }
+}
