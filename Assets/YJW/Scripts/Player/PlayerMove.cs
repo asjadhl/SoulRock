e4efa8593a9b369dfe30064e43d8be93baf22696
LEE SANGHYUK <dltkdguer@gmail.com>
2025-10-10 21:53:20 +0900

    Sky

--- Assets/YJW/Scripts/Player/PlayerMove.cs
+++ Assets/YJW/Scripts/Player/PlayerMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     private Rigidbody playerRb;
-    private float moveSpeed = 1f;
+    private float moveSpeed = 10f;
 
     //float playerJumpForce = 13f;
 
--- Assets/YJW/Scripts/Stage1Boss/BossMove.cs
+++ Assets/YJW/Scripts/Stage1Boss/BossMove.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class BossMove : MonoBehaviour
 {
-   private float moveSpeed = 1f;
+   private float moveSpeed = 10f;
 
     private void Update()
     {
