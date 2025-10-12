6bd76dbd344937abfb5a003dc75aed6f0cd2b39d
LEE SANGHYUK <dltkdguer@gmail.com>
2025-10-13 04:21:57 +0900

    MUSICBOX

---
+++ Assets/LSH/Scripts/Dialog/DialogManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    private string[] Dialog;
+    [SerializeField] Text text;
+    bool isDialog = true;
+    void Start()
+    {
+        Dialog = new string[]
+        {
+            "HEHEH",
+            "BAAAAAAAH",
+            "WELCOME TO JACKSON SHOW",
+            "UUUUUUUUUUUUUUUU"
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MainDialog();
+    }
+
+    void MainDialog()
+    {
+        if (isDialog)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (Dialog.Length > 0)
+                {
+                    text.text = Dialog[0];
+                    var temp = new string[Dialog.Length - 1];
+                    for (int i = 0; i < temp.Length; i++)
+                    {
+                        temp[i] = Dialog[i + 1];
+                    }
+                    Dialog = temp;
+                }
+                else
+                {
+                    text.text = "";
+                    isDialog = false;
+                }
+            }
+
+        }
+    }
+}
