ad057aacdc35eb8b3164308cbb440ed39aaf3287
asjadhl <yunjiwoo0518@gmail.com>
2025-10-24 12:04:14 +0900

    1

--- Assets/YJW/Scripts/Player/PlayerShoot.cs
+++ Assets/YJW/Scripts/Player/PlayerShoot.cs
@@ -1,5 +1,7 @@
 using Cysharp.Threading.Tasks;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem.DualShock.LowLevel;
@@ -17,102 +19,87 @@ public class PlayerShoot : MonoBehaviour
     BossMove bossMove;
     private Vector3 originalPosition;
 
-    private void Update()
-    {
-        //    PlayerShoot_();
-    }
+    private Dictionary<string, Action<Collider>> _tagActions;
+
+
     void Start()
     {
         originalPosition = gunTransform.localPosition;
         bossMove = GameObject.FindAnyObjectByType<BossMove>();
-	}
+        InitializeTagActions();
+    }
+
+    private void Update()
+    {
+        //    PlayerShoot_();
+    }
+
+    private void InitializeTagActions()
+    {
+        _tagActions = new Dictionary<string, Action<Collider>>
+        {
+            { "Dummy", col => {
+                var dummy = col.GetComponent<DummySpawner>();
+                dummy.getDummyHit = true;
+                dummy.getDummyDamage();
+            }},
+            { "Mirror", col => col.GetComponent<MatarialAlpha>().mirrorRotate() },
+            { "LazerBall", col => col.gameObject.SetActive(false) },
+            { "Skull", col => col.GetComponent<MiniSkullMove>().ShootReturnSkull() },
+            { "Stage2Boss", col => {
+                var bossAttack = boss2.GetComponent<Stage2BossAttack>();
+                if (bossAttack.curShape == Shape.D)
+                    bossAttack.playerHitCount++;
+            }},
+            { "RedCard", col => col.GetComponent<CardMove>().CardGetDam() },
+            { "GoldCard", col => col.GetComponent<CardMove>().CardGetDam() },
+            { "miniH", col => col.GetComponent<MiniBoss>().ReturnOriPos() },
+            { "GhostBall", col => col.GetComponent<LastBossMove>().HitGhostBoss() },
+            { "Poltergeist", col => col.gameObject.SetActive(false) },
+            { "FakeClone", col => col.GetComponent<CloneMove>().OnHit() },
+            { "miniHTrue", col => col.GetComponent<MiniBoss>().miniHTrue() },
+            { "Wheel", col => boss2.GetComponent<Stage2BossAttack>().WheelStop() },
+            { "RealClone", col => {
+                var clone = col.GetComponent<GBAttack>();
+                clone.ReturnClone();
+                clone.SuccessFindRealClone();
+            }},
+        };
+    }
 
 
     public void PlayerShoot_()
     {
-        if (Input.GetMouseButtonDown(0))
+        StartCoroutine(Kickback());
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 100f))
         {
-            //_=GunMove();
-            StopAllCoroutines();
-            StartCoroutine(Kickback());
-
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit, 100f))
-            {
-                if (hit.collider.gameObject.tag == "Dummy") //요것만 주석처리하면댐
-                {
-                    hit.collider.gameObject.GetComponent<DummySpawner>().getDummyHit = true;
-                    hit.collider.gameObject.GetComponent<DummySpawner>().getDummyDamage();
-                }
-                if (hit.collider.gameObject.tag == "Mirror")
-                {
-                    hit.collider.gameObject.GetComponent<MatarialAlpha>().mirrorRotate();
-                }
-
-				if (hit.collider.gameObject.tag == "LazerBall")
-				{
-                    hit.collider.gameObject.SetActive(false);
-				}
-                if (hit.collider.gameObject.tag == "Skull")
-                    hit.collider.gameObject.GetComponent<MiniSkullMove>().ShootReturnSkull();
-                if (hit.collider.gameObject.tag == "Stage2Boss" && boss2.GetComponent<Stage2BossAttack>().curShape == Shape.D)
-                {
-                    boss2.GetComponent<Stage2BossAttack>().playerHitCount++;
-                }
-                if (hit.collider.gameObject.tag == "RedCard" || hit.collider.gameObject.tag == "GoldCard")
-                {
-                    hit.collider.gameObject.GetComponent<CardMove>().CardGetDam();
-                }
-                if (hit.collider.gameObject.tag == "miniH")
-                {
-                    hit.collider.gameObject.GetComponent<MiniBoss>().ReturnOriPos();
-                }
-                if(hit.collider.gameObject.tag == "GhostBall")
-                {
-                    hit.collider.gameObject.GetComponent<LastBossMove>().HitGhostBoss();
-                }
-                if(hit.collider.gameObject.tag == "Poltergeist")
-                {
-                    hit.collider.gameObject.SetActive(false);
-                }
-                if(hit.collider.gameObject.tag == "FakeClone")
-                {
-                    hit.collider.gameObject.GetComponent<CloneMove>().OnHit();
-                }
-                if (hit.collider.gameObject.tag == "miniHTrue")
-                {
-                    hit.collider.gameObject.GetComponent<MiniBoss>().miniHTrue();
-                }
-                if(hit.collider.gameObject.tag == "Wheel")
-                {
-                    boss2.GetComponent<Stage2BossAttack>().WheelStop();
-                }
-                if(hit.collider.gameObject.tag == "RealClone")
-                {
-                    hit.collider.gameObject.GetComponent<GBAttack>().ReturnClone();
-					hit.collider.gameObject.GetComponent<GBAttack>().SuccessFindRealClone();
-				}
-
-            }
-
-
-            // Health.cs
-            if (Physics.Raycast(ray, out hit, 100f))
-            {
-
-                if (hit.collider.TryGetComponent<IDamagable>(out IDamagable c))
-                {
-                    c?.TakeHit(8);
-                }
-                else
-                {
-                    hit.collider.GetComponentInChildren<IDamagable>()?.TakeHit(8);
-                }
-            }
+            HandleHit(hit.collider);
         }
     }
+
+    private void HandleHit(Collider col)
+    {
+        if (col == null) return;
+
+        // 태그 기반 행동
+        if (_tagActions.TryGetValue(col.tag, out var action))
+        {
+            action.Invoke(col);
+        }
+
+        // IDamagable 처리 (공통 데미지)
+        if (col.TryGetComponent<IDamagable>(out IDamagable damageable))
+        {
+            damageable.TakeHit(8);
+        }
+        else
+        {
+            col.GetComponentInChildren<IDamagable>()?.TakeHit(8);
+        }
+    }
+
     private IEnumerator Kickback()
     {
         shootParticle.Play();
