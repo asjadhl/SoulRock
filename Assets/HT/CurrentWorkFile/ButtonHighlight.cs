4bfc55ca9b37ebbaeee851ba14f19acf77398554
GodDr02 <swonkim64@gmail.com>
2025-10-28 15:09:40 +0900

    UI

--- Assets/HT/CurrentWorkFile/ButtonHighlight.cs
+++ Assets/HT/CurrentWorkFile/ButtonHighlight.cs
@@ -1,36 +1,46 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-using Cysharp.Threading.Tasks;
 
 public class ButtonHighlight : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
-{
+{   
+
+
+  public enum State
+  {
+     Pressed,Hovering,ReturningToHovering,ReturningToEnd,EnteringToHovering,Waiting
+  };
+
+  public float Rotation;
     private Material mat;
     private Button myButton;
-    private bool isPressed = false;
-    private bool isHovered = false;
-
+    public State mystate;
+ 
     float Deg = 0;
     float duration = 1.3f;
-
+    float value = 0;
+   
+     float sinevalue = 0;
+  
+ 
     void Awake()
     {
+     
         myButton = GetComponent<Button>();
 
         // Get and clone the material so this button has its own instance
         mat = Instantiate(GetComponent<Image>().material);
         GetComponent<Image>().material = mat;
-
+    mat.SetFloat("_Rotation", Rotation);
+    mystate = State.Waiting;
         // Hook into the button click
         myButton.onClick.AddListener(OnClick);
     }
 
     void Update()
     {
-        if (isHovered && !isPressed)
-        {
-            SinUpdate();
-        }
+    StateUpdate();
+      
     }
 
     void SinUpdate()
@@ -40,33 +50,113 @@ public class ButtonHighlight : MonoBehaviour, IPointerEnterHandler, IPointerExit
 
         float rad = Mathf.Deg2Rad * Deg;
 
-        float alpha = Mathf.Lerp(0.3f, 0.5f, (Mathf.Sin(rad) + 1f) * 0.5f);
+       value = Mathf.Lerp(0.3f, 0.5f, (Mathf.Sin(rad) + 1f) * 0.5f);
+          
+    
 
-        mat.SetFloat("_value", alpha);
+        mat.SetFloat("_value", value);
     }
+     
 
+    void StateUpdate()
+    {
+      switch(mystate)
+      { 
+        case State.EnteringToHovering:
+        {
+          Deg += (360f / duration) * Time.unscaledDeltaTime;
+          if (Deg > 360f) Deg -= 360f;
+
+          float rad = Mathf.Deg2Rad * Deg;
+          sinevalue = Mathf.Lerp(0.3f, 0.5f, (Mathf.Sin(rad) + 1f) * 0.5f);
+
+          if (Mathf.Abs(value - sinevalue) > 0.01f)
+            value = Mathf.MoveTowards(value, sinevalue, 1 * Time.unscaledDeltaTime);
+          else
+            mystate = State.Hovering;
+
+          mat.SetFloat("_value", value);
+        }
+        break;
+        case State.Hovering:
+        {
+          Deg += (360f / duration) * Time.unscaledDeltaTime;
+          if (Deg > 360f) Deg -= 360f;
+
+          float rad = Mathf.Deg2Rad * Deg;
+          sinevalue = Mathf.Lerp(0.3f, 0.5f, (Mathf.Sin(rad) + 1f) * 0.5f);
+          value = sinevalue;
+
+          mat.SetFloat("_value", value);
+        }
+        break;
+        case State.Pressed:
+        {
+          value = Mathf.MoveTowards(value, 1, 5f * Time.unscaledDeltaTime);
+          mat.SetFloat("_value", value);
+
+          if(Mathf.Abs(value-1) < 0.01f)
+          {
+            value = 1;
+            mystate = State.ReturningToHovering;
+          }
+        }
+        break;
+      case State.ReturningToHovering:
+        {
+          Deg += (360f / duration) * Time.unscaledDeltaTime;
+          if (Deg > 360f) Deg -= 360f;
+
+          float rad = Mathf.Deg2Rad * Deg;
+          sinevalue = Mathf.Lerp(0.3f, 0.5f, (Mathf.Sin(rad) + 1f) * 0.5f);
+
+          if (Mathf.Abs(value - sinevalue) > 0.01f)
+          {
+            value = Mathf.MoveTowards(value, sinevalue, 1 * Time.unscaledDeltaTime);
+          }
+          else
+            mystate = State.Hovering;
+
+          mat.SetFloat("_value", value);
+        }
+        break;
+      case State.ReturningToEnd:
+        {
+          if (Mathf.Abs(value - 0) > 0.01f)
+            value = Mathf.MoveTowards(value,0,1*Time.unscaledDeltaTime);
+          else
+          {
+            value = 0;
+            mystate = State.Waiting;
+          }
+          mat.SetFloat("_value", value);
+        }
+        break;
+        case State.Waiting:
+        {
+          // NULL
+        }
+        break;
+      }
+    }
     void OnClick()
     {
-        // Example: set shader property when clicked
-        mat.SetFloat("_value", 1);
-        isPressed = true;
-       Wait(0.2f).Forget();
+   
+    mystate = State.Pressed;
+  
     }
-   async UniTaskVoid Wait(float delay)
-   {
-    await UniTask.WaitForSeconds(delay);
-    isPressed = false;
-   }
+  
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if (!isPressed)
-            isHovered = true;
+    
+
+    mystate = State.EnteringToHovering;
     }
 
     public void OnPointerExit(PointerEventData eventData)
-    {
-            isPressed = false;
-            isHovered = false;
-        mat.SetFloat("_value", 0);
+    {     
+
+     mystate = State.ReturningToEnd;
+           
     }
 }
